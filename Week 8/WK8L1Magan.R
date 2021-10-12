a4cdfa8458e2bd3eac230c1a535a1ca02e03e17f
Hank Magan <bryant.magan@gmail.com>
2021-10-12 16:54:19 -0400

    Started lab

---
+++ Week 8/WK8L1Magan.R
@@ -0,0 +1,33 @@
+---
+title: "PCA"
+author: "Hank Magan"
+date: "10/11/2021"
+abstract: "hi"
+output: pdf_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+## Introduction
+
+## Pharmacokinetics and Pharmacodynamics
+
+Within the field of pharmacology, there are two main studies: pharmacokinectics and pharmacodynamics. From a high level standpoint, pharmokinectics is the study of how an organism affects a drug. It is studied alongside pharmodynamics (which will be discussed in-depth shortly) in order to determine a variety of descriptors, such as drug dosing in commerical settings. When a drug (or any substance, really) is consumed, highly specific metabolic reactions occur, digesting and transforming the consumed drug. This is accomplished via enzymes, which, again, are highly specific. For example: lactase is responsible for digesting lactose (milk sugar), cellulase is responsible for digesting cellulose (plant cell wall), etc. Of course, the exact way that this digestion (or "breaking down") process occurs varies from enzyme to enzyme, and therefore drug to drug as ezymes are specific to their substrates. With that said, there are a few general steps that can be applied to better understand the digestion process, even if it is unique to different substances. These steps are characterized by the acronym ADME, which is explained in more detail below: 
+
+A. Absorption  
+
+B. Distribution
+
+C. Metabolism 
+
+D. Execretion  
+
+## Principle Component Analysis (PCA)
+
+## Methods
+
+## Results 
+
+## Conclusion
